1f72a7e369e2a83e8b77c257b5665f0744c962ec
ZyphDoz <zyphdoz@users.noreply.github.com>
2024-03-24 06:22:17 +0100

    remove code generated by vite template

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './styles/App.css'
 import DateTimePicker from './DateTimePicker'
 import { DateAndTime } from './types/DateAndTime'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   const time: DateAndTime = {
     year: "",
