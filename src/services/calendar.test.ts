89af6760730aac334f33a7508ed517ca12e41329
ZyphDoz <zyphdoz@users.noreply.github.com>
2024-04-07 07:22:02 +0200

    start refactoring calendar tasks to use a hashmap because the current approach that has to sort and traverse the entire array is very slow when you have a lot of tasks

--- src/services/calendar.test.ts
+++ src/services/calendar.test.ts
@@ -1,28 +1,27 @@
-import { expect, test } from "vitest";
-import { calendar, CalendarTask } from "./calendar";
+import { expect, test } from 'vitest';
+import { calendar, CalendarTask } from './calendar';
 
-test("calendar.addTask(key, task)_whenGivenAKeyAndTask_shouldAddTask", () => {
-  const task: CalendarTask = {
-    title: "Test Task",
-    description: "This is a test task",
-    category: "Test",
+const testTask: CalendarTask = {
+    title: 'Test Task',
+    description: 'This is a test task',
+    category: 'Test',
     startTime: {
-      year: "2024",
-      month: "April",
-      day: "6",
-      hour: "10",
-      minute: "30",
-      date: new Date("2024-04-06T10:30:00"),
+        year: '2024',
+        month: '03',
+        day: '30',
+        hour: '22',
+        minute: '00',
+        date: new Date('2024-03-30T22:00:00'),
     },
     endTime: {
-      year: "2024",
-      month: "April",
-      day: "6",
-      hour: "11",
-      minute: "30",
-      date: new Date("2024-04-06T11:30:00"),
+        year: '2024',
+        month: '03',
+        day: '31',
+        hour: '04',
+        minute: '00',
+        date: new Date('2024-03-31T04:00:00'),
     },
-    taskDuration: 60,
+    taskDuration: 360,
     taskId: 1,
     numRepeats: 0,
     repeatMonday: false,
@@ -32,9 +31,16 @@ test("calendar.addTask(key, task)_whenGivenAKeyAndTask_shouldAddTask", () => {
     repeatFriday: false,
     repeatSaturday: false,
     repeatSunday: false,
-  };
+};
 
-  calendar.addTask(new Date("2024-04-06T10:30:00"), task);
-  const tasks = calendar.getAllTasks();
-  expect(tasks[new Date("2024-04-06T10:30:00").toString()]).toEqual(task);
+test('calendar.addTask(task)_whenGivenATask_shouldAddTaskToHashMap', () => {
+    calendar.addTask(testTask);
+    const tasks = calendar.getAllTasks();
+    const { year, month, day } = testTask.startTime;
+    expect(tasks[`${month}/${day}/${year}`][0]).toEqual(testTask);
+});
+
+test('calendar.getKeyFromTask_whenGivenATask_shouldReturnTaskDateAs MM/dd/yyyy string', () => {
+    // @ts-ignore
+    expect(calendar.getKeyFromTask(testTask)).toEqual('03/30/2024');
 });
--- src/services/calendar.ts
+++ src/services/calendar.ts
@@ -1,41 +1,53 @@
 export interface DateAndTime {
-  year: string;
-  month: Month;
-  day: Day;
-  hour: Hour;
-  minute: Minute;
-  date: Date;
+    year: string;
+    month: string;
+    day: string;
+    hour: string;
+    minute: string;
+    date: Date;
 }
 
 export interface CalendarTask {
-  title: string;
-  description: string;
-  category: string;
-  startTime: DateAndTime;
-  endTime: DateAndTime;
-  taskDuration: number;
-  taskId: number;
-  numRepeats: number;
-  repeatMonday: boolean;
-  repeatTuesday: boolean;
-  repeatWednesday: boolean;
-  repeatThursday: boolean;
-  repeatFriday: boolean;
-  repeatSaturday: boolean;
-  repeatSunday: boolean;
+    title: string;
+    description: string;
+    category: string;
+    startTime: DateAndTime;
+    endTime: DateAndTime;
+    taskDuration: number;
+    taskId: number;
+    numRepeats: number;
+    repeatMonday: boolean;
+    repeatTuesday: boolean;
+    repeatWednesday: boolean;
+    repeatThursday: boolean;
+    repeatFriday: boolean;
+    repeatSaturday: boolean;
+    repeatSunday: boolean;
 }
 
 interface CalendarMap<T> {
-  [key: string]: T;
+    [key: string]: T;
 }
 
-const tasks: CalendarMap<CalendarTask> = {};
+class Calendar {
+    private tasks: CalendarMap<CalendarTask[]> = {};
 
-export const calendar = {
-  addTask: function (key: Date, task: CalendarTask) {
-    tasks[key.toString()] = task;
-  },
-  getAllTasks: function () {
-    return tasks;
-  },
-};
+    addTask(task: CalendarTask) {
+        const key = this.getKeyFromTask(task).toString();
+        if (!this.tasks[key]) {
+            this.tasks[key] = [];
+        }
+        this.tasks[key].push(task);
+    }
+
+    getAllTasks() {
+        return this.tasks;
+    }
+
+    private getKeyFromTask(task: CalendarTask) {
+        const { month, day, year } = task.startTime;
+        return `${month}/${day}/${year}`;
+    }
+}
+
+export const calendar = new Calendar();
