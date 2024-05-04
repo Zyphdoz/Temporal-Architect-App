76c8e6df82f909cf030e3072ad713ed823aec525
ZyphDoz <zyphdoz@users.noreply.github.com>
2024-05-04 06:28:34 +0200

    add tailwind to the project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam omnis consequatur ex aspernatur, dicta
+                officiis? Vitae explicabo ullam nulla officia modi temporibus possimus, quam obcaecati natus. Nobis
+                veniam explicabo modi.
+            </h1>
+        </>
+    );
+}
+
+export default App;
