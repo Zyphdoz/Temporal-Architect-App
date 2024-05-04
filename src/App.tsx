459be6b0654b7e7dbde73baf0bf2081952f05975
ZyphDoz <zyphdoz@users.noreply.github.com>
2024-05-04 11:17:52 +0200

    add sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import SidebarMenu from './components/SidebarMenu';
+
 function App() {
     return (
         <>
+            <SidebarMenu></SidebarMenu>
             <h1 className="text-3xl font-bold underline">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam omnis consequatur ex aspernatur, dicta
                 officiis? Vitae explicabo ullam nulla officia modi temporibus possimus, quam obcaecati natus. Nobis
---
+++ src/components/SidebarMenu.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import logo from '../assets/logo.png';
+
+function SidebarMenu() {
+    const menuItems: string[] = ['Calendar', 'Statistics', 'Settings'];
+    const [selectedMenuItem, setSelectedMenuItem] = useState(menuItems[0]);
+
+    return (
+        <aside className="h-screen w-56">
+            <nav className="flex h-full flex-col border-r bg-gray-100 shadow-sm">
+                <div className="flex items-center justify-between p-4 pb-2">
+                    <img
+                        src={logo}
+                        className="w-20 rounded-full"
+                        alt="A minimalistic logo with a clock and some abstract shapes, symbolizing the precision and intentionality of a temporal architect’s approach to time management"
+                    ></img>
+                    <h1 className="rounded-md bg-white p-1.5">Temporal Architect App</h1>
+                </div>
+
+                <ul className="mt-4 flex-1 px-4 checked:bg-gray-200">
+                    {menuItems.map((menuItem, i) => (
+                        <li
+                            key={i}
+                            className={`my-1 cursor-pointer rounded-md px-4 py-2 hover:bg-gray-200 ${selectedMenuItem === menuItem ? 'bg-gray-200' : ''}`}
+                            onClick={() => setSelectedMenuItem(menuItem)}
+                        >
+                            <label className="cursor-pointer">
+                                <input
+                                    type="radio"
+                                    value={menuItem}
+                                    checked={selectedMenuItem === menuItem}
+                                    onChange={() => setSelectedMenuItem(menuItem)}
+                                    className="hidden"
+                                ></input>
+                                {menuItem}
+                            </label>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </aside>
+    );
+}
+
+export default SidebarMenu;
