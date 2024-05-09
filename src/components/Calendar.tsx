6f03b7629291371b8740cfe53109db673a8bba80
ZyphDoz <zyphdoz@users.noreply.github.com>
2024-05-09 16:19:20 +0200

    add buttons for changing calendar view to next/previous day

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -26,19 +26,41 @@ function Calendar() {
 
     return (
         <div>
-            <div className="relative border-b bg-gray-50 pb-3 pt-6 text-center text-4xl shadow-sm">
-                <label htmlFor="selecteddate" className="cursor-pointer pr-2">
+            <div className="relative border-b bg-gray-50 pb-3 pt-6 text-left text-4xl shadow-sm">
+                <button
+                    id="previousday"
+                    className={`mx-3 hover:font-bold`}
+                    onClick={() =>
+                        setSelectedDate((prevDate) => {
+                            return addDays(prevDate, -1);
+                        })
+                    }
+                >
+                    <span className="flex -translate-x-3 justify-center">{'〈'}</span>
+                </button>
+                <button
+                    id="nextday"
+                    className={`mx-2 hover:font-bold`}
+                    onClick={() =>
+                        setSelectedDate((prevDate) => {
+                            return addDays(prevDate, 1);
+                        })
+                    }
+                >
+                    <span className="flex translate-x-3 justify-center">{'〉'}</span>
+                </button>
+                <label htmlFor="showdatepicker" className="mx-2 cursor-pointer pr-2">
                     {getMonthName(selectedDate)} {selectedDate.getFullYear()}
                 </label>
                 <button
-                    id="selecteddate"
+                    id="showdatepicker"
                     className={`h-10 w-10 rounded-lg border bg-gray-100 hover:bg-gray-200 ${datePickerIsOpen && 'bg-gray-200'}`}
                     onClick={() => setDatePickerIsOpen(!datePickerIsOpen)}
                 >
                     <span className="flex -translate-y-1 justify-center">⌄</span>
                 </button>
                 {datePickerIsOpen && (
-                    <div className="absolute left-1/2 z-50 -translate-x-1/2 transform">
+                    <div className="absolute  z-50 translate-x-20 transform">
                         <DatePicker
                             selected={selectedDate}
                             onChange={handleChange}
