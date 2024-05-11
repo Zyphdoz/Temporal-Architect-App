675c8114d83629ed6c10028fbd916118c0500f39
ZyphDoz <zyphdoz@users.noreply.github.com>
2024-05-11 17:32:17 +0200

    add ability to open task editor by clicking on task

--- src/components/CalendarTask.tsx
+++ src/components/CalendarTask.tsx
@@ -1,4 +1,5 @@
 import { CalendarTask as CalendarTaskType } from '../services/calendar';
+import { taskEditor } from '../services/taskEditor';
 import { dateToHhMmIn24hFormat } from '../utils/dateAndTimeUtils';
 
 interface CalendarTaskProps {
@@ -15,6 +16,7 @@ function CalendarTask({ tasks }: CalendarTaskProps) {
                     <div
                         className="transform cursor-pointer transition-all duration-200 hover:opacity-80 hover:shadow-lg"
                         key={index}
+                        onClick={() => taskEditor.open()}
                     >
                         <div style={{ height, backgroundColor, boxSizing: 'border-box' }}>
                             <div className="flex justify-between px-4 pb-2 pt-3">
